5e23272377f44f29cc731d7aac46ee91e520e7c6
Ariska Keldermann <ariska.keldermann@essent.nl>
2022-03-22 13:07:15 +0100

    Stacked graph

--- src/app/graph/data.ts
+++ src/app/graph/data.ts
@@ -1,20 +1,73 @@
 export const data = {
   chart: {
-    caption: 'Countries With Most Oil Reserves [2017-18]',
-    subCaption: 'In MMbbl = One Million barrels',
-    xAxisName: 'Country',
-    yAxisName: 'Reserves (MMbbl)',
-    numberSuffix: 'K',
+    caption: 'Verbruik per jaar',
+    subCaption: '',
+    xAxisname: 'Jaar',
+    yAxisName: 'Kosten',
+    numberPrefix: '€',
     theme: 'fusion',
   },
-  data: [
-    { label: 'Venezuela', value: '290' },
-    { label: 'Saudi', value: '260' },
-    { label: 'Canada', value: '180' },
-    { label: 'Iran', value: '140' },
-    { label: 'Russia', value: '115' },
-    { label: 'UAE', value: '100' },
-    { label: 'US', value: '30' },
-    { label: 'China', value: '30' },
+  categories: [
+    {
+      category: [
+        {
+          label: '2018',
+        },
+        {
+          label: '2019',
+        },
+        {
+          label: '2020',
+        },
+        {
+          label: '2021',
+        },
+        {
+          label: '2022',
+        },
+      ],
+    },
+  ],
+  dataset: [
+    {
+      seriesname: 'Electriciteit',
+      data: [
+        {
+          value: '1200.50',
+        },
+        {
+          value: '1000',
+        },
+        {
+          value: '1350',
+        },
+        {
+          value: '1235',
+        },
+        {
+          value: '1450',
+        },
+      ],
+    },
+    {
+      seriesname: 'Gas',
+      data: [
+        {
+          value: '1314',
+        },
+        {
+          value: '131.4',
+        },
+        {
+          value: '1548.66',
+        },
+        {
+          value: '983',
+        },
+        {
+          value: '131',
+        },
+      ],
+    },
   ],
 };
